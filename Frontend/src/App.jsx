29dfb6df555ffce4b6c961ff3e282d2cff1a8e09
Preeti-Chavda <chavdapriti2001@gmail.com>
2024-05-06 01:14:29 +0530

    Frontend done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Home from './Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import Courses from './Courses/Courses';
+import SignUp from './components/SignUp';
+import Contact from './components/Contact';
+
+
+function App() {
+  return (
+    <>
+      <div className='dark:bg-slate-900 dark:text-white'>
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/course' element={<Courses/>} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+        
+
+    </>
+  )
+}
+
+export default App;
